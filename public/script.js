d67bb4fa8542c4672c51cb1c7b3364759383925d
M A Razvi <abuzarflw@gmail.com>
2022-05-17 01:46:43 -0700

    Add project carousel

--- public/script.js
+++ public/script.js
@@ -1,1 +1,21 @@
 console.log("yessirr");
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:45,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    stagePadding: 0,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
